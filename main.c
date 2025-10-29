b7358cd26ce61c1bafbaeb660e6b50e05c52f08f
C3mR4de <NVadim2005@gmail.com>
2025-10-30 01:31:22 +0300

    feat: add MVP in main.c

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
+#include "Simulation.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_SOURCES  3
+#define NUM_DEVICES  3
+#define BUFFER_SIZE  2
+#define MIN_INTERVAL 5.0
+#define MAX_INTERVAL 15.0
+#define SERVICE_RATE 0.1
+#define MAX_TIME     100
+
 int main()
 {
+    Simulation* simulation = Simulation_Create
+    (
+        NUM_SOURCES,
+        NUM_DEVICES,
+        BUFFER_SIZE,
+        MIN_INTERVAL,
+        MAX_INTERVAL,
+        SERVICE_RATE,
+        MAX_TIME
+    );
+
+    if (!simulation)
+        return EXIT_FAILURE;
+
+    while (Simulation_Step(simulation));
+    Simulation_Destroy(simulation);
+
     return EXIT_SUCCESS;
 }
