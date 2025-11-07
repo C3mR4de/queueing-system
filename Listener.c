483096d9032214315b40c82bcf7c523334d9896d
C3mR4de <NVadim2005@gmail.com>
2025-11-07 21:36:09 +0300

    fix: temporarily ignore methods arguments

--- Listener.c
+++ Listener.c
@@ -5,7 +5,7 @@
 
 struct Listener
 {
-
+    int a;
 };
 
 Listener* Listener_Create(void)
@@ -20,10 +20,14 @@ void Listener_Destroy(Listener* const listener)
 
 void Listener_OnStep(Listener* const listener, const size_t step, const TimeMoment time, const char* message, Vector* const devices, const Buffer* const buffer)
 {
+    (void)listener;
+    (void)devices;
+    (void)buffer;
     printf("Шаг %zu (t = %" PRId32 "): %s\n", step, time, message);
 }
 
-void Listener_OnFinish(Listener* const Listener)
+void Listener_OnFinish(Listener* const listener)
 {
+    (void)listener;
     puts("=== Моделирование завершено ===");
 }
